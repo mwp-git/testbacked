bb99941288ce12ca9da0f2134490b812f636768e
mwp-git <80424857+mwp-git@users.noreply.github.com>
2021-03-10 21:15:46 +0100

    Add files via upload

---
+++ PostComponent.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div>
+    <h1>Submit data to API</h1>
+    <form @submit="postData" mothod="post">
+      <input type="text" name="author" placeholder="author" v-model="posts.author"><br><br>
+      <input type="text" name="title" placeholder="title" v-model="posts.title"><br><br>
+      <button type="submit">Post</button>
+    </form>
+  </div>
+
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+const app = Vue.createApp(App)
+app.use(VueAxios, axios)
+//Skal lige have hjælp.
+
+export default{
+  name:"PostComponent",
+  data(){
+    return{
+      posts:{
+        title:null,
+        author:null
+      }
+    }
+  },
+
+  methods:{
+    postData(e){
+      console.warn(this.posts)
+      e.preventDefault()
+      }
+    }
+}
+</script>
